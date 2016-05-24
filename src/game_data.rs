3ec4d9d3e59e3e1dfe3c106fbb1af5b83c3d8cad
Nate Jones <jonesnl@umich.edu>
2016-05-23 23:30:56 -0400

    Add memory functions

--- src/game_data.rs
+++ src/game_data.rs
@@ -36,3 +36,47 @@ impl Memory {
         }
     }
 }
+
+impl Bus for Memory {
+    fn write8(&mut self, addr: BusWidth, data: u8) {
+        match addr {
+            0x0000...0x7FFF => {
+                (*self.cartridge).write8(addr, data);
+            },
+            0x8000...0x9FFF => {
+                self.vram[(addr-0x8000) as usize] = data;
+            },
+            0xA000...0xBFFF => {
+                (*self.cartridge).write8(addr, data);
+            },
+            0xC000...0xDFFF => {
+                self.wram[(addr-0xC000) as usize] = data;
+            },
+            0xE000...0xFDFF => {
+                self.wram[(addr-0xE000) as usize] = data;
+            },
+            0xFE00...0xFE9F => {
+                self.oam[(addr-0xFE00) as usize] = data;
+            },
+            0xFEA0...0xFEFF => {
+                panic!("Unusable memory address {}", addr);
+            },
+            0xFF00...0xFF7F => {
+                // I/O
+            },
+            0xFF80...0xFFFE => {
+                self.hram[(addr-0xFF80) as usize] = data;
+            },
+            0xFFFF => {
+                // Interrupt enable register
+            },
+            _ => {
+                panic!("Illegal write to {}", addr);
+            }
+        };
+    }
+
+    fn read8(&self, addr: BusWidth) -> u8 {
+        0
+    }
+}

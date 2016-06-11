45f36f2abcd7ed9de5a03bb8799edc1ff79f6ad1
Nate Jones <jonesnl@umich.edu>
2016-06-11 18:35:26 -0400

    Some work on register accessing

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,31 +1,60 @@
+#![allow(dead_code)]
 
+use std::mem;
+
+// XXX Think about this some more.
+// Would it be cleaner to just use reader/writer functions?
+// Pro: No unsafe block
+// Con: Much more duplicated code (maybe a macro could avoid the duplication?)
+pub enum IndReg {
+    A = 1,
+    F = 0,
+    B = 3,
+    C = 2,
+    D = 5,
+    E = 4,
+    H = 7,
+    L = 6,
+}
+
+pub enum CombReg {
+    AF = 0,
+    BC = 1,
+    DE = 2,
+    HL = 3,
+}
 
 pub struct Cpu {
-    reg_a: u8,
-    reg_f: u8,
-    reg_b: u8,
-    reg_c: u8,
-    reg_d: u8,
-    reg_e: u8,
-    reg_h: u8,
-    reg_l: u8,
-    sp: u16,
-    pc: u16,
+    pub reg: [u16; 4],
+    pub sp: u16,
+    pub pc: u16,
 }
 
 impl Cpu {
     pub fn new() -> Cpu {
         Cpu {
-            reg_a: 0,
-            reg_f: 0,
-            reg_b: 0,
-            reg_c: 0,
-            reg_d: 0,
-            reg_e: 0,
-            reg_h: 0,
-            reg_l: 0,
+            reg: [0u16; 4],
             sp: 0,
             pc: 0,
         }
     }
+
+    pub fn reg_ind(&mut self, reg: IndReg) -> &mut u8 {
+        unsafe {
+            let u8_vec: &mut [u8; 8] = mem::transmute(&mut self.reg);
+            &mut u8_vec[reg as usize]
+        }
+    }
+
+    pub fn reg_comb(&mut self, reg: CombReg) -> &mut u16 {
+        &mut self.reg[reg as usize]
+    }
+}
+
+#[test]
+fn basic_reg_test() {
+    let mut cpu = Cpu::new();
+    *cpu.reg_ind(IndReg::A) = 0x1;
+    assert!(*cpu.reg_ind(IndReg::A) == 0x1);
+    assert!(*cpu.reg_comb(CombReg::AF) == 0x100);
 }
